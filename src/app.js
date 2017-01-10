4c791b64a403f3b6dddd277d0095075ec9f17376
FTROOT\skesepara <salihkesepara@gmail.com>
2017-01-10 15:49:09 +0300

    initial commit

---
+++ src/app.js
@@ -0,0 +1,5 @@
+let msg = 'welcome to my ap'
+  console.log(msg)
+
+document.write(msg)
+console.log('app loaded')
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+var path = require('path')
+
+module.exports = {
+  entry: {
+    app: [
+      'webpack-dev-server/client?http://localhost:3000/',
+      './src/app.js'
+    ]
+  },
+  output: {
+    path: path.resolve(__dirname, '/'),
+    filename: '/dist/bundle.js'
+  },
+  module: {
+    loaders: [
+      {test: /\.js$/, exclude: /(node_modules|bower_components)/, loader: 'babel-loader', query: {presets: ['es2015']}}
+    ]
+  },
+  
+  devtool: "eval"
+}
